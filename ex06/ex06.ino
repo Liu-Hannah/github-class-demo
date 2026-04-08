1525498c25fd6f57e2f0a42424542de59f0a1c13
Liu-Hannah <1849640549@qq.com>
2026-04-08 21:42:16 +0800

    Add lab06

--- ex06/ex06.ino
+++ ex06/ex06.ino
@@ -7,18 +7,18 @@ const int resolution = 8;
 void setup() {
   Serial.begin(115200);
 
-  // 直接用你实验3里的写法，绑定两个通道
   ledcAttach(ledPinA, freq, resolution);
   ledcAttach(ledPinB, freq, resolution);
+
 }
 
 void loop() {
-  // 灯A从0→255变亮，灯B同步从255→0变暗
+  // 灯A从0→255变亮，灯B同步从255→0变暗d
   for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){
     int brightnessB = 255 - dutyCycle;
     ledcWrite(ledPinA, dutyCycle);
     ledcWrite(ledPinB, brightnessB);
-    delay(15); // 控制速度，越小越快
+    delay(15); 
   }
 
   // 反向循环，让灯A从255→0变暗，灯B从0→255变亮
